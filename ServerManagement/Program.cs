7bddf8253b4e506f48702866e4287f0991db859d
Vahag <vahagnlvoskanyan@gmail.com>
2026-06-12 15:19:39 +0400

    Static, Interactive Routing. Multiple Directives. Optional Parameters. Query Strings. NotFound in NavigationManager.

--- ServerManagement/Program.cs
+++ ServerManagement/Program.cs
@@ -18,6 +18,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+// Overrides browser's default 404 page with our own Razor Component page.
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
