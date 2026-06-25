ac77bab634b3fdcd38f8fd62fa492c4ca7efcc1f
Vahag <vahagnlvoskanyan@gmail.com>
2026-06-25 16:14:11 +0400

    Web Assembly

---
+++ WebAssemblyDemo/WebAssemblyDemo.Client/ContainerStorage.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAssemblyDemo.Client
+{
+    public class ContainerStorage
+    {
+        private string _message = string.Empty;
+
+        public string Message
+        {
+            get => _message;
+            set => _message = value;
+        }
+    }
+}
